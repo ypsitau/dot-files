8489c2e4e7def5c01d1b68acfb915dde54bab008
ypsitau <ypsitau@nifty.com>
2014-09-16 18:09:02 +0900

    implementing gura-mode

--- .emacs.d/gura-mode.el
+++ .emacs.d/gura-mode.el
@@ -1,9 +1,7 @@
-(defun gura-mode ()
-  "Gura Mode"
-  (interactive)
-  (kill-all-local-variables)
-  (setq mode-name "Gura")
-  (setq major-mode 'gura-mode)
-  (run-hooks 'gura-mode-hook))
+(define-derived-mode gura-mode prog-mode "Gura"
+  "Major mode for editing Gura programming language."
+  ;;(set-syntax-table gura-mode-syntax-table)
+  ;;(setq-local font-lock-defaults '(gura-mode-font-lock-keywords nil nil))
+  )
 
 (provide 'gura-mode)
