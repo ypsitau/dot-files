274c0e3514bc0a5a3528f1cbf086de5cbbb8d384
ypsitau <ypsitau@nifty.com>
2014-09-17 19:41:59 +0900

    added gura-mode

--- .emacs.d/gura-mode.el
+++ .emacs.d/gura-mode.el
@@ -5,57 +5,55 @@
 	map)
   "Keymap for `gura-mode'.")
 
-(defvar gura-mode-syntax-table
-  (let ((st (make-syntax-table)))
-	(modify-syntax-entry ?# "<" st)
-	(modify-syntax-entry ?\n ">" st)
-	st)
-  "Syntax table for `gura-mode'.")
+(progn
+  (defconst gura-font-lock-defaults
+	'(
+	  ("\\<\\(if\\|elsif\\|else\\)\\>" . font-lock-keyword-face)
+	  ("\\<\\(try\\|catch\\)\\>" . font-lock-keyword-face)
+	  ("\\<\\(repeat\\|while\\|cross\\)\\>" . font-lock-keyword-face)
+	  ("\\<\\(break\\|continue\\|return\\)\\>" . font-lock-keyword-face)
+	  ("\\<\\(module\\|class\\|import\\)\\>" . font-lock-keyword-face)
+	  ("\\<\\(true\\|false\\|nil\\)\\>" . font-lock-constant-face)
+	  ("\\<\\(\\w+\\)\\>" . font-lock-variable-name-face)
+	  ))
 
-(defconst gura-mode-keywords-regexp
-  (regexp-opt '("if" "while")))
-
-(defvar gura-font-lock-keywords
-  cpp-font-lock-keywords)
-
-(define-derived-mode gura-mode prog-mode "Gura"
-  "Major mode for editing Gura programming language."
-  ;;(set-syntax-table gura-mode-syntax-table)
-  (set (make-local-variable 'font-lock-defaults) '(gura-font-lock-keywords))
-  (set (make-local-variable 'indent-line-function) 'gura-indent-line)
-  )
+  (define-derived-mode gura-mode prog-mode "Gura"
+	"Major mode for editing Gura programming language."
+	(modify-syntax-entry ?_ "w" gura-mode-syntax-table)
+	(modify-syntax-entry ?/ ". 124b" gura-mode-syntax-table)
+	(modify-syntax-entry ?* ". 23" gura-mode-syntax-table)
+	(modify-syntax-entry ?\n "> b" gura-mode-syntax-table)
+	(set (make-local-variable 'font-lock-defaults) '(gura-font-lock-defaults))))
 
 (defun gura-indent-line ()
   "Indent current line of Gura code."
   (interactive)
-  (let* ((savep (point))
-	 (indent (condition-case nil
-		     (save-excursion
-		       (forward-line 0)
-		       (skip-chars-forward " \t")
-		       (if (>= (point) savep) (setq savep nil))
-		       (max (asm-calculate-indentation) 0))
-		   (error 0))))
-    (if savep
-	(save-excursion (indent-line-to indent))
-      (indent-line-to indent))))
-
-(defun asm-calculate-indentation ()
-  (or
-   ;; Flush labels to the left margin.
-   (and (looking-at "\\(\\sw\\|\\s_\\)+:") 0)
-   ;; Same thing for `;;;' comments.
-   (and (looking-at "\\s<\\s<\\s<") 0)
-   ;; Simple `;' comments go to the comment-column.
-   (and (looking-at "\\s<\\(\\S<\\|\\'\\)") comment-column)
-   ;; The rest goes at the first tab stop.
-   (or (car tab-stop-list) tab-width)))
-
-;;  (let ((savep (> (current-column) (current-indentation)))
-;;		(indent (condition-case nil (max (sample-calculate-indentation) 0)
-;;				  (error 0))))
-;;	(if savep
-;;		(save-excursion (indent-line-to indent))
-;;	  (indent-line-to indent))))
+  (beginning-of-line)
+  (if (bobp)
+	  (indent-line-to 0)
+	(let ((not-indented t) cur-indent)
+	  (if (looking-at "^[ \t]*END_")
+		  (progn
+			(save-excursion
+			  (forward-line -1)
+			  (setq cur-indent (- (current-indentation) default-tab-width)))
+			(if (< cur-indent 0)
+				(setq cur-indent 0)))
+		(save-excursion
+		  (while not-indented
+			(forward-line -1)
+			(if (looking-at "^[ \t]*END_")
+				(progn
+				  (setq cur-indent (current-indentation))
+				  (setq not-indented nil))
+			  (if (looking-at "^[ \t]*\\(PARTICIPANT\\|MODEL\\|APPLICATION\\|WORKFLOW\\|ACTIVITY\\|DATA\\|TOOL_LIST\\|TRANSITION\\)")
+				  (progn
+					(setq cur-indent (+ (current-indentation) default-tab-width))
+					(setq not-indented nil))
+				(if (bobp)
+					(setq not-indented nil)))))))
+	  (if cur-indent
+		  (indent-line-to cur-indent)
+		(indent-line-to 0)))))
 
 (provide 'gura-mode)
