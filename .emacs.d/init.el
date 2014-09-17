cec1622713e66a30168b1d9f23af37ed8943b7e7
ypsitau <ypsitau@nifty.com>
2014-09-17 11:59:31 +0900

    updated init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -23,12 +23,13 @@
  '(cursor ((((class color) (background dark)) (:background "#000000")) (((class color) (background light)) (:background "#ffffff")) (t nil))))
 
 (global-set-key "\C-m" 'newline-and-indent)
-(global-set-key "\C-x\C-a" 'beginning-of-buffer)
-(global-set-key "\C-x\C-e" 'end-of-buffer)
+;;(global-set-key "\C-x\C-a" 'beginning-of-buffer)
+;;(global-set-key "\C-x\C-e" 'end-of-buffer)
 (global-set-key "\C-x\C-p" 'scroll-down)
 (global-set-key "\C-x\C-n" 'scroll-up)
 (global-set-key "\C-x\C-l" 'compile)
 (global-set-key "\C-x\C-j" 'goto-line)
+(global-set-key "\C-x\C-y" 'foo)
 
 (setq-default tab-width 4)
 
