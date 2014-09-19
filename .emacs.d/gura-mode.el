3fd3a1791ffe51340737c1758b12a54eda019ff9
ypsitau <ypsitau@nifty.com>
2014-09-19 11:22:36 +0900

    updated gura-mode

--- .emacs.d/gura-mode.el
+++ .emacs.d/gura-mode.el
@@ -1,124 +1,116 @@
 ;;; gura-mode.el --- Gura major mode
 
+(makunbound 'gura-hoge)
+(makunbound 'gura-font-lock-keywords)
+(makunbound 'gura-font-lock-syntactic-keywords)
+(makunbound 'gura-mode-syntax-table)
+(makunbound 'gura-imenu-generic-expression)
+(makunbound 'gura-outline-regexp)
 
-(progn
-  (defvar gura-mode-map
-	(let ((map (make-sparse-keymap)))
-	  map)
-	"Keymap for gura-mode.")
+(defun gura-brace-close ()
+  (interactive)
+  (insert "**"))
 
-  (setq gura-font-lock-keywords
-		`(
-		  ;; function names for control sequence
-		  (,(rx symbol-start
-				(or
-				 "if" "elsif" "else" "try" "catch"
-				 "repeat" "while" "cross" "break" "continue" "return"
-				 "module" "class" "import"
-				 ) symbol-end)
-		   (0 font-lock-keyword-face))
-		  ;; constant variables
-		  (,(rx symbol-start
-				(or
-				 "true" "false" "nil"
-				 ) symbol-end)
-		   (0 font-lock-constant-face))
-		  ;; symbol name
-		  (,(rx "`" (1+ word))
-		   (0 font-lock-variable-name-face))
-		  ;; function name
-		  (,(rx symbol-start (group (1+ word)) (0+ space) "(")
-		   (1 font-lock-function-name-face))
-		  ;; top-level assignment
-		  (,(rx line-start (group (1+ word)) (0+ space) "=")
-		   (1 font-lock-variable-name-face))))
+(defvar gura-mode-map
+  (let ((map (make-sparse-keymap)))
+	;;(define-key map "}"		'gura-brace-close)
+	map)
+  "Keymap for `gura-mode'.")
 
-  (setq gura-font-lock-syntactic-keywords
-		`(
-		  ("^\\(=\\)\\sw" (1 "< b"))
-		  ))
+(defvar gura-mode-syntax-table
+  (let ((st (make-syntax-table)))
+	(modify-syntax-entry ?' "\"" st)
+	(modify-syntax-entry ?_ "w" st)
+	(modify-syntax-entry ?$ "w" st)
+	;; comment syntax
+	(modify-syntax-entry ?# "<" st)
+	(modify-syntax-entry ?/ ". 124" st)
+	(modify-syntax-entry ?* ". 23b" st)
+	(modify-syntax-entry ?\n ">" st)
+	st)
+  "Syntax table for `gura-mode'.")
 
-  (defun gura-indent-line ()
-	"Indent current line of Gura code."
-	(interactive)
-	(beginning-of-line)
-	(let ((indent-to-set nil))
-	  (if (looking-at ".*}")
-		  (save-excursion
-			(forward-line -1)
-			(if (looking-at ".*{")
-				(setq indent-to-set (current-indentation))
-			  (setq indent-to-set (- (current-indentation) default-tab-width))))
+(defvar gura-font-lock-keywords
+  `(
+	;; function names for control sequence
+	(,(rx symbol-start
+		  (or
+		   "if" "elsif" "else" "try" "catch"
+		   "repeat" "while" "cross" "break" "continue" "return"
+		   "module" "class" "import"
+		   ) symbol-end)
+	 (0 font-lock-keyword-face))
+	;; constant variables
+	(,(rx symbol-start
+		  (or
+		   "true" "false" "nil"
+		   ) symbol-end)
+	 (0 font-lock-constant-face))
+	;; symbol name
+	(,(rx "`" (1+ word))
+	 (0 font-lock-variable-name-face))
+	;; function name
+	(,(rx symbol-start (group (1+ word)) (0+ space) "(")
+	 (1 font-lock-function-name-face))
+	;; top-level assignment
+	(,(rx line-start (group (1+ word)) (0+ space) "=")
+	 (1 font-lock-variable-name-face)))
+  "Keyword highlighting specification for `gura-mode'.")
+
+(defvar gura-imenu-generic-expression
+  )
+
+(defvar gura-outline-regexp
+  )
+
+(define-derived-mode gura-mode fundamental-mode "Gura"
+  "Major mode for editing Gura programming language."
+  :syntax-table gura-mode-syntax-table
+  (use-local-map (nconc (make-sparse-keymap) gura-mode-map))
+  (set (make-local-variable 'indent-line-function) 'gura-indent-line)
+  (set (make-local-variable 'font-lock-defaults)
+	   '(gura-font-lock-keywords
+		 nil nil nil nil
+		 (font-lock-syntactic-keywords . gura-font-lock-syntactic-keywords))))
+
+(defun gura-indent-line ()
+  "Indent current line of Gura code."
+  (interactive)
+  (let ((savep (> (current-column) (current-indentation)))
+		(indent (condition-case nil (max (gura-calculate-indentation) 0)
+				  (error 0))))
+	(if savep
+		(save-excursion (indent-line-to indent))
+	  (indent-line-to indent))))
+
+(defun gura-calculate-indentation ()
+  "Return the column to which the current line should be indented."
+  (let ((indent-to-set (current-indentation)))
+	(if (looking-at ".*}")
 		(save-excursion
-		  (let ((continue-flag t))
-			(while continue-flag
-			  (forward-line -1)
-			  (if (looking-at ".*}")
+		  (forward-line -1)
+		  (if (looking-at ".*{")
+			  (setq indent-to-set (current-indentation))
+			(setq indent-to-set (- (current-indentation) default-tab-width))))
+	  (save-excursion
+		(let ((continue-flag t))
+		  (while continue-flag
+			(forward-line -1)
+			(if (looking-at ".*}")
+				(progn
+				  (setq indent-to-set (current-indentation))
+				  (setq continue-flag nil))
+			  (if (looking-at ".*{")
 				  (progn
-					(setq indent-to-set (current-indentation))
-					(setq continue-flag nil))
-				(if (looking-at ".*{")
-					(progn
-					  (setq indent-to-set (+ (current-indentation) default-tab-width))
-					  (setq continue-flag nil))))
-			  (if (bobp) (setq continue-flag nil)))
-			)))
-	  (if indent-to-set (indent-line-to (max 0 indent-to-set)))
-	  ))
-
-  (define-derived-mode gura-mode fundamental-mode "Gura"
-	"Major mode for editing Gura programming language."
-	(let ((st gura-mode-syntax-table))
-	  (modify-syntax-entry ?' "\"" st)
-	  (modify-syntax-entry ?_ "w" st)
-	  (modify-syntax-entry ?$ "w" st)
-	  ;; comment syntax
-	  (modify-syntax-entry ?# "<" st)
-	  (modify-syntax-entry ?/ ". 124" st)
-	  (modify-syntax-entry ?* ". 23b" st)
-	  (modify-syntax-entry ?\n ">" st))
-	(set (make-local-variable 'indent-line-function) 'gura-indent-line)
-	(set (make-local-variable 'font-lock-defaults)
-		 '(gura-font-lock-keywords
-		   nil nil nil nil
-		   (font-lock-syntactic-keywords . gura-font-lock-syntactic-keywords)))))
+					(setq indent-to-set (+ (current-indentation) default-tab-width))
+					(setq continue-flag nil))))
+			(if (bobp) (setq continue-flag nil)))
+		  )))
+	indent-to-set))
 
 (defun foo ()
   (interactive)
-  ;;(print (current-indentation)))
   (gura-indent-line))
-
-
-(defun gura-indent-line-1 ()
-  "Indent current line of Gura code."
-  (interactive)
-  (beginning-of-line)
-  (if (bobp)
-	  (indent-line-to 0)
-	(let ((not-indented t) cur-indent)
-	  (if (looking-at "}")
-		  (progn
-			(save-excursion
-			  (forward-line -1)
-			  (setq cur-indent (- (current-indentation) default-tab-width)))
-			(if (< cur-indent 0)
-				(setq cur-indent 0)))
-		(save-excursion
-		  (while not-indented
-			(forward-line -1)
-			(if (looking-at "}")
-				(progn
-				  (setq cur-indent (current-indentation))
-				  (setq not-indented nil))
-			  (if (looking-at "{")
-				  (progn
-					(setq cur-indent (+ (current-indentation) default-tab-width))
-					(setq not-indented nil))
-				(if (bobp)
-					(setq not-indented nil)))))))
-	  (if cur-indent
-		  (indent-line-to cur-indent)
-		(indent-line-to 0)))))
 
 (add-to-list 'auto-mode-alist '("\\.gura$" . gura-mode))
 (add-to-list 'auto-mode-alist '("\\.guraw$" . gura-mode))
