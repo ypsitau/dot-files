7bd4b8f1069389fdacc37fab8e8173dd43cf04c9
ypsitau <ypsitau@nifty.com>
2015-12-17 11:58:12 +0900

    updated autoexec.bat

--- autoexec.bat
+++ autoexec.bat
@@ -3,7 +3,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 set DIRCMD=/OG /ON
 path=%path%;C:\Program Files (x86)\TeraPad
 path=C:\Users\ypsitau\Documents\gura\bin-x86;%path%
-path=%path%;C:\Users\ypsitau\Documents\gura\guest\buildtools-mswin\7za920
-path=%path%;C:\Users\ypsitau\Documents\gura\guest\buildtools-mswin\curl
-path=%path%;C:\Users\ypsitau\Documents\gura\guest\buildtools-mswin\UnxUtils
-path=%path%;C:\Users\ypsitau\Documents\gura\guest\buildtools-mswin\wix38-binaries
+path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\7za920
+path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\curl
+path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\UnxUtils
+path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\wix38-binaries
