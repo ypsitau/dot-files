a93e548a46fa6c056fcd5bbc1c5122290a4c23ce
ypsitau <ypsitau@nifty.com>
2016-02-16 19:55:11 +0900

    modified path

--- autoexec.bat
+++ autoexec.bat
@@ -2,7 +2,7 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\vcvarsall.bat"
 set DIRCMD=/OG /ON
 path=%path%;C:\Program Files (x86)\TeraPad
-path=%path%;%USERPROFILE%\Documents\gura\bin-x86
+path=%USERPROFILE%\Documents\gura\bin-x86;%path%
 path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\7za920
 path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\curl
 path=%path%;%USERPROFILE%\Documents\gura\guest\buildtools-mswin\UnxUtils
