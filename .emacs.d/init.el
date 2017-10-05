d23de9b6b9c17e611e410d8a507617811efb9880
ypsitau <ypsitau@nifty.com>
2017-10-05 10:47:20 +0900

    added emmet-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -81,3 +81,12 @@
             (auto-fill-mode 1)
             (if (eq window-system 'x)
                 (font-lock-mode 1))))
+
+(add-to-list 'load-path "~/.emacs.d/site-lisp")
+
+(require 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+(add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
+;(add-hook 'emmet-mode-hook (lambda () (setq emmet-indent-after-insert nil)))
+;(add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2))) ;; indent 2 spaces.
+(setq emmet-self-closing-tag-style " /") ;; default "/"
