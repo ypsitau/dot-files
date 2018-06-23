23ee2f7e4261e7f0a8ccd16b9d9aba26bc2d53b3
ypsitau <ypsitau@nifty.com>
2018-06-23 17:41:55 +0900

    uses vs2017

--- autoexec.bat
+++ autoexec.bat
@@ -1,6 +1,6 @@
 @echo off
-set VCVERSION=14.0
-call "C:\Program Files (x86)\Microsoft Visual Studio %VCVERSION%\VC\vcvarsall.bat"
+set VCVERSION=2017
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd.bat" -arch=x64
 set DIRCMD=/OG /ON
 path=%USERPROFILE%\gura\bin-x86;%path%
 path=%path%;C:\Program Files (x86)\TeraPad
