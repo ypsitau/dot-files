75149372d3fb07cd18f304d0be49f8d918bd8da9
Yutaka Saito <ypsitau@nifty.com>
2020-03-21 15:23:29 +0900

    updated autoexec.bat

--- autoexec.bat
+++ autoexec.bat
@@ -1,6 +1,6 @@
 @echo off
 set VCVERSION=2017
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\vsdevcmd.bat" -arch=x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" -arch=x86
 set DIRCMD=/OG /ON
 set GURAPATH=msc
 path=%USERPROFILE%\gura\bin-x86;%path%
@@ -13,6 +13,7 @@ path=%path%;%USERPROFILE%\gura\guest\buildtools-mswin\wix38-binaries
 path=%path%;%USERPROFILE%\bin
 path=%path%;%USERPROFILE%\bin\VJR200_131
 path=%path%;%USERPROFILE%\bin\jrasm
+path=%path%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd
 doskey gura-archive=gura -S archive.gura $*
 doskey gura-genmod=gura -S genmod.gura $*
 doskey gura-genclass=gura -S genclass.gura $*
