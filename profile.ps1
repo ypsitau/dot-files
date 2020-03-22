39d4ab8c3c2870504fdcdcf46b85e3329c216170
ypsitau <ypsitau@nifty.com>
2020-03-22 12:50:35 +0900

    updated PowerShell profile

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,3 @@
 $Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\"
+$Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
+$Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
