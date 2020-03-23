635103b9fc977efa13f196bcd622aad7b8458dbc
ypsitau <ypsitau@nifty.com>
2020-03-23 12:17:19 +0900

    updated profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,3 +1,12 @@
+#==============================================================================
+# User Profile for Power Shell
+# Create a file that has a name indicated by $Profile with the code below:
+#   . "C:\Users\ypsit\dot-files\profile.ps1"
+#==============================================================================
 $Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\"
 $Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
 $Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
+
+function prompt {
+    "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))";
+}
