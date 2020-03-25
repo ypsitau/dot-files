4dc873e3ca5c91d299c64de79101fe2d00f79cfe
ypsitau <ypsitau@nifty.com>
2020-03-25 22:01:54 +0900

    added search path

--- profile.ps1
+++ profile.ps1
@@ -1,11 +1,16 @@
 #==============================================================================
 # User Profile for Power Shell
-# Create a file that has a name indicated by $Profile with the code below:
-#   . "C:\Users\ypsit\dot-files\profile.ps1"
+# Set the link target of PowerShell icon as follows:
+#   PowerShell -NoLogo -NoExit -File Profile.ps1
 #==============================================================================
-$Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\"
-$Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
-$Env:Path += ";C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
+$installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath
+Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
+Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation
+
+$Env:Path += ";C:\Program Files\7-zip\"
+$Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\"
+$Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
+$Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
 
 function prompt {
     "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))";
