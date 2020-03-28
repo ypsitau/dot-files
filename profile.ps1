c07757d531d6d60e5d0c4c698d106c37e88536f4
ypsitau <ypsitau@nifty.com>
2020-03-28 21:07:22 +0900

    added path

--- profile.ps1
+++ profile.ps1
@@ -8,9 +8,10 @@ Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevS
 Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation
 
 $Env:Path += ";C:\Program Files\7-zip\"
-$Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\cmd\"
+$Env:Path += ";C:\Program Files\git\bin\"
 $Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
 $Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
+$Env:Path += ";${HOME}\source\gurax\out\install\x64-Release\bin\"
 
 function prompt {
     "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))";
