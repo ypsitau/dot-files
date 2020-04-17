d8fad1876c34cb856f5542d1e0ea7e3a93684bc7
ypsitau <ypsitau@nifty.com>
2020-04-17 13:49:49 +0900

    implementing PowerShell script

---
+++ def-function.ps1
@@ -0,0 +1,21 @@
+#==============================================================================
+# User functions for Power Shell
+#==============================================================================
+function prompt {
+    "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))";
+}
+
+function gurax-genclass { gurax -S genclass.gura $args }
+function gurax-genmod { gurax -S genmod.gura $args }
+
+function build {
+    if (-not(Test-Path "build")) { cmake . -G Ninja -B build }
+    ninja -C build $args
+}
+
+function build-Release {
+    if (-not(Test-Path "build-Release")) {
+        cmake . -G Ninja -B build-Release -DCMAKE_BUILD_TYPE=Release
+    }
+    ninja -C build-Release $args
+}
--- profile.ps1
+++ profile.ps1
@@ -7,7 +7,6 @@ $installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswher
 Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
 Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation -DevCmdArguments -arch=x64
 
-#$Env:GURAX_DIRBASE = "${Home}\source\gurax"
 $Env:GURAX_PATH = "build;build-Release"
 
 $Env:Path += ";C:\Program Files\7-zip\"
@@ -16,21 +15,4 @@ $Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake
 $Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
 $Env:Path += ";${HOME}\Source\gurax\bin\"
 
-function prompt {
-    "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))";
-}
-
-function gurax-genclass { gurax -S genclass.gura $args }
-function gurax-genmod { gurax -S genmod.gura $args }
-
-function build {
-    if (-not(Test-Path "build")) { cmake . -G Ninja -B build }
-    ninja -C build $args
-}
-
-function build-Release {
-    if (-not(Test-Path "build-Release")) {
-        cmake . -G Ninja -B build-Release -DCMAKE_BUILD_TYPE=Release
-    }
-    ninja -C build-Release $args
-}
+. ./def-functions.ps1
