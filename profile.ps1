50b5c6e24d6ad4a247edeacdccebf00e8214d495
ypsitau <ypsitau@nifty.com>
2020-05-02 12:22:32 +0900

    updated profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,18 +1,20 @@
 #==============================================================================
 # User Profile for Power Shell
 # Set the link target of PowerShell icon as follows:
-#   PowerShell /NoLogo /NoExit /File Profile.ps1
+#   powershell.exe -NoLogo -NoExit -File Profile.ps1
 #==============================================================================
-$installPath = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath
-Import-Module (Join-Path $installPath "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
-Enter-VsDevShell -VsInstallPath $installPath -SkipAutomaticLocation -DevCmdArguments -arch=x64
+$dirNameVsDev = &"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -version 16.0 -property installationpath
+Import-Module (Join-Path $dirNameVsDev "Common7\Tools\Microsoft.VisualStudio.DevShell.dll")
+Enter-VsDevShell -VsInstallPath $dirNameVsDev -SkipAutomaticLocation -DevCmdArguments -arch=x64
 
 $Env:GURAX_PATH = "build;build-Release"
 
 $Env:Path += ";C:\Program Files\7-zip\"
 $Env:Path += ";C:\Program Files\git\bin\"
-$Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
-$Env:Path += ";${installPath}\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
-$Env:Path += ";${HOME}\Source\gurax\bin\"
+$Env:Path += ";$dirNameVsDev\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\"
+$Env:Path += ";$dirNameVsDev\Common7\IDE\CommonExtensions\Microsoft\TeamFoundation\Team Explorer\Git\usr\bin\"
+$Env:Path += ";$HOME\Source\gurax\bin\"
+#$Env:Path += ";$HOME\Source\gurax\build\src\gurax\"
+#$Env:Path += ";$HOME\Source\gurax\build\src\lib\"
 
-. ${HOME}\dot-files\def-function.ps1
+. $PSScriptRoot\def-function.ps1
