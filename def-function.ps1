1188c980abd6e198c49aeb822937ba6de71fa47c
ypsitau <ypsitau@nifty.com>
2020-08-23 14:19:20 +0900

    implemented create-patch-vs

--- def-function.ps1
+++ def-function.ps1
@@ -5,10 +5,11 @@ function prompt {
     "$($executionContext.SessionState.Path.CurrentLocation)$('>' * ($nestedPromptLevel + 1))";
 }
 
-function gurax-genclass { gurax -S genclass.gura $args }
-function gurax-genmod { gurax -S genmod.gura $args }
 function gurax-base64encode { gurax -S base64encode.gura $args }
 function gurax-base64decode { gurax -S base64decode.gura $args }
+function gurax-diff { gurax -S diff-s.gura $args }
+function gurax-genclass { gurax -S genclass.gura $args }
+function gurax-genmod { gurax -S genmod.gura $args }
 
 function build {
     if (-not(Test-Path "build")) { cmake . -G Ninja -B build }
@@ -20,4 +21,14 @@ function build-Release {
         cmake . -G Ninja -B build-Release -DCMAKE_BUILD_TYPE=Release
     }
     ninja -C build-Release $args
+}
+
+function create-patch-vs {
+    Param([parameter(mandatory)][string]$zip, [parameter(mandatory)][string]$path)
+    $pathNames = Foreach ($fileName in (Get-ChildItem -path $path -recurse -include ('*.vcxproj', '*.sln') -name)) {
+        Join-Path ($path -replace "^.\\","") $fileName
+    }
+    echo $pathNames
+    if (Test-Path $zip) { del $zip }
+    7z a $zip $pathNames
 }
